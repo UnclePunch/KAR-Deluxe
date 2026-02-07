2fe9f6fd63f7b046d965a3fbc31ae4252b8487ec
UnclePunch <vinbertinelli@gmail.com>
2026-02-07 02:21:37 -0500

    menu: fix multiplayer result screens

--- mods/wide/src/menu.c
+++ mods/wide/src/menu.c
@@ -31,23 +31,42 @@ CODEPATCH_HOOKCREATE(0x8013f89c, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
 CODEPATCH_HOOKCREATE(0x801404f0, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
 CODEPATCH_HOOKCREATE(0x80140084, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
 
+// air ride results splitscreen
+CODEPATCH_HOOKCREATE(0x801426a0, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
+CODEPATCH_HOOKCREATE(0x80142e94, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
 
 // city results
 CODEPATCH_HOOKCREATE(0x801438f0, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
 CODEPATCH_HOOKCREATE(0x80144cbc, "mr 3, 30\t\n", Menu_ShiftText, "", 0)
 CODEPATCH_HOOKCREATE(0x80145544, "mr 3, 31\t\n", Menu_ShiftText, "", 0)
 
+// city results splitscreen
+CODEPATCH_HOOKCREATE(0x80143f90, "mr 3, 27\t\n", Menu_ShiftText, "", 0)
+CODEPATCH_HOOKCREATE(0x8014515c, "mr 3, 30\t\n", Menu_ShiftText, "", 0)
+CODEPATCH_HOOKCREATE(0x801444e0, "mr 3, 31\t\n", Menu_ShiftText, "", 0) // points
+CODEPATCH_HOOKCREATE(0x80145c58, "mr 3, 26\t\n", Menu_ShiftText, "", 0)
+
 void MainMenuAdjust_Init()
 {
     CODEPATCH_HOOKAPPLY(0x80153d88);
     CODEPATCH_HOOKAPPLY(0x8015e79c);
 
+    
     CODEPATCH_HOOKAPPLY(0x8013f89c);
     CODEPATCH_HOOKAPPLY(0x801404f0);
     CODEPATCH_HOOKAPPLY(0x80140084);
 
+    CODEPATCH_HOOKAPPLY(0x801426a0);
+    CODEPATCH_HOOKAPPLY(0x80142e94);
+
+
     CODEPATCH_HOOKAPPLY(0x801438f0);
     CODEPATCH_HOOKAPPLY(0x80144cbc);
     CODEPATCH_HOOKAPPLY(0x80145544);
+    
+    CODEPATCH_HOOKAPPLY(0x80143f90);
+    CODEPATCH_HOOKAPPLY(0x8014515c);
+    CODEPATCH_HOOKAPPLY(0x801444e0);
+    CODEPATCH_HOOKAPPLY(0x80145c58);
     
 }
